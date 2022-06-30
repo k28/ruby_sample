6274d9f61e6e7d1d1c9fd08b2e25b0a5c0b4f0f8
Kazuya Hatano <k28@me.com>
2022-07-01 05:46:08 +0900

    update

--- tips.rb
+++ tips.rb
@@ -7,3 +7,10 @@ require "prime"
 primes = Prime.each(100).select{|i| i}
 puts primes.inspect
 
+require 'bigdecimal'
+
+# 大きな数を文字列に変換する(指数表現でなく)
+b = BigDecimal(637924140000000000)
+p b.to_s('F')  # 637924140000000000.0
+p b.floor.to_s # 637924140000000000
+
